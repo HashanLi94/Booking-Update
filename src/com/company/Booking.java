4671caeee6267e682fc0e99dc9574d9874580d69
Hashan Liyanage <55444962+HashanLi94@users.noreply.github.com>
2020-05-01 11:44:46 +0530

    commit

--- src/com/company/Booking.java
+++ src/com/company/Booking.java
@@ -1,9 +1,52 @@
 package com.company;
 
+import java.util.List;
+
 public class Booking {
     int bookigId;
     int paxCount;
-    int date;
-    int timeSlot;
+    String  date;
+    String timeSlot;
 
+    public Booking(){
+
+    }
+
+    public Booking(int bookigId, int paxCount, String date, String  timeSlot){
+        this.bookigId = bookigId;
+        this.paxCount = paxCount;
+        this.date = date;
+        this.timeSlot = timeSlot;
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "bookigId=" + bookigId +
+                ", paxCount=" + paxCount +
+                ", date='" + date + '\'' +
+                ", timeSlot='" + timeSlot + '\'' +
+                '}';
+    }
+
+    //get all the bookings
+    public List<Booking> getAllBookings(List<Booking> booking){
+        return booking;
+    }
+
+    //get a booking bt id
+    public Booking getBoookingById(int bId, List<Booking> booking){
+
+        Booking x = null;
+//                new Booking();
+//        logic
+        for (int i=0; i<booking.size(); i++){
+            if(bId == booking.get(i).bookigId){
+                x = booking.get(i);
+
+            }//else System.out.println(bId + " is not matching");
+        }
+        System.out.println(bId + " Succsses");
+        return x;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,64 @@
 package com.company;
 
+import javafx.css.converter.LadderConverter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EmptyStackException;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Access to Booking class methods, attributes
+        Booking booking = new Booking();
+
+        //Booking data objects
+//        Booking b1 = new Booking(001, 2, "2020/03/31", "12:15");
+//        Booking b2 = new Booking(002, 2, "2020/04/09", "14:15");
+//        Booking b3 = new Booking(003, 4, "2020/03/17", "16:00");
+//        Booking b4 = new Booking(004, 6, "2020/04/21", "20:15");
+//        Booking b5 = new Booking(005, 7, "2020/04/20", "17:15");
+
+
+        //convert the booking objects to a booking list method 1
+//        List<Booking> bookings = new ArrayList<>();
+//        bookings.add(b1);
+//        bookings.add(b2);
+//        bookings.add(b3);
+//        bookings.add(b4);
+//        bookings.add(b5);
+
+        //convert the booking objects to a booking list method 2
+//        List<Booking> bookings = new ArrayList<>(
+//                Arrays.asList(
+//                          new Booking(001, 2, "2020/03/31", "12:15"),
+//                            new Booking(002, 2, "2020/04/09", "14:15"),
+//                            new Booking(003, 4, "2020/03/17", "16:00"),
+//                            new Booking(004, 6, "2020/04/21", "20:15"),
+//                            new Booking(005, 7, "2020/04/20", "17:15")
+//                )
+//        );
+
+        //convert the booking objects to a booking list method 3
+        List<Booking> bookings = Arrays.asList(
+                new Booking(001, 2, "2020/03/31", "12:15"),
+                new Booking(002, 2, "2020/04/09", "14:15"),
+                new Booking(003, 4, "2020/03/17", "16:00"),
+                new Booking(004, 6, "2020/04/21", "20:15"),
+                new Booking(005, 7, "2020/04/20", "17:15")
+        );
+
+        //get all bookings
+        List<Booking> allResult;
+        allResult=  booking.getAllBookings(bookings);
+        System.out.println(allResult);
+
+        //get booking byId
+        Booking resultById;
+        resultById = booking.getBoookingById(002, bookings);
+        System.out.println(resultById);
+
     }
 }
